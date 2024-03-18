ac7de0a7e06cb985f1cc92958af2cad8ce9b377e
Alessandro Tedesco <alessandrotedd@gmail.com>
2024-03-18 21:33:44 +0100

    Move files to Chess folder

---
+++ Assets/Chess/Scripts/ChessUtils.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MRTemplateAssets.Scripts {
+    public class ChessUtils : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
