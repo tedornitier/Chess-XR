fed122f3785d894fb971701202df8baad0f81948
Alessandro Tedesco <alessandrotedd@gmail.com>
2024-03-19 17:53:18 +0100

    Add GetPieceCoordinateFromCellTest

--- Assets/Tests/ChessUtilsTests.cs
+++ Assets/Tests/ChessUtilsTests.cs
@@ -24,5 +24,36 @@ public class ChessUtilsTests
         position = new Vector3(3.9f, 0, -4f);
         result = ChessUtils.CalculateCellPosition(position, playAreaLength);
         Assert.AreEqual((7, 0), result);
+
+        playAreaLength = 16;
+        position = new Vector3(-8, 0, -8);
+        result = ChessUtils.CalculateCellPosition(position, playAreaLength);
+        Assert.AreEqual((0, 0), result);
+    }
+
+    [Test]
+    public void GetPieceCoordinateFromCellTest()
+    {
+        float playAreaLength = 8;
+
+        (int, int) cell = (0, 0);
+        (double, double) result = ChessUtils.GetPieceCoordinateFromCell(cell, playAreaLength);
+        Assert.AreEqual((-3.5, -3.5), result);
+
+        cell = (7, 7);
+        result = ChessUtils.GetPieceCoordinateFromCell(cell, playAreaLength);
+        Assert.AreEqual((3.5, 3.5), result);
+
+        cell = (7, 0);
+        result = ChessUtils.GetPieceCoordinateFromCell(cell, playAreaLength);
+        Assert.AreEqual((3.5, -3.5), result);
+
+        cell = (0, 7);
+        result = ChessUtils.GetPieceCoordinateFromCell(cell, playAreaLength);
+        Assert.AreEqual((-3.5, 3.5), result);
+
+        cell = (2, 5);
+        result = ChessUtils.GetPieceCoordinateFromCell(cell, playAreaLength);
+        Assert.AreEqual((-1.5, 1.5), result);
     }
 }
