c916103e1c3ad2dad4cac53a8de7023b6b216b91
Alessandro Tedesco <alessandrotedd@gmail.com>
2024-03-19 19:23:23 +0100

    Assign chess piece color

--- Assets/Chess/Scripts/ChessPiece.cs
+++ Assets/Chess/Scripts/ChessPiece.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class ChessPiece : MonoBehaviour
 {
     [SerializeField]
     public ChessType type;
+    [SerializeField]
+    public ChessColor color;
 
     public void ChessPieceSelected() {
         Debug.Log("ChessPiece - ChessPieceSelected");
--- Assets/Chess/Scripts/Game.cs
+++ Assets/Chess/Scripts/Game.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Game : MonoBehaviour
+public class Game : MonoBehaviour // TODO this should be the chessboard class
 {
     [SerializeField]
     private GameObject chessBoard;
