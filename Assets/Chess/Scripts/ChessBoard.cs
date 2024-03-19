1607265a66430702fccd42e5ef3894550f7dd3b1
Alessandro Tedesco <alessandrotedd@gmail.com>
2024-03-20 00:12:27 +0100

    Use setter in SetupBoard

--- Assets/Chess/Scripts/ChessBoard.cs
+++ Assets/Chess/Scripts/ChessBoard.cs
@@ -35,28 +35,27 @@ public class ChessBoard
     public void SetupBoard()
     {
         board = new ChessPiece[8, 8];
+        SetPieceAt(0, 0, new ChessPiece(ChessType.Rook, ChessColor.Black, 0, 0));
+        SetPieceAt(1, 0, new ChessPiece(ChessType.Knight, ChessColor.Black, 1, 0));
+        SetPieceAt(2, 0, new ChessPiece(ChessType.Bishop, ChessColor.Black, 2, 0));
+        SetPieceAt(3, 0, new ChessPiece(ChessType.Queen, ChessColor.Black, 3, 0));
+        SetPieceAt(4, 0, new ChessPiece(ChessType.King, ChessColor.Black, 4, 0));
+        SetPieceAt(5, 0, new ChessPiece(ChessType.Bishop, ChessColor.Black, 5, 0));
+        SetPieceAt(6, 0, new ChessPiece(ChessType.Knight, ChessColor.Black, 6, 0));
+        SetPieceAt(7, 0, new ChessPiece(ChessType.Rook, ChessColor.Black, 7, 0));
         for (int x = 0; x < 8; x++)
         {
-            board[x, 1] = new ChessPiece(ChessType.Pawn, ChessColor.Black, x, 1);
-            board[x, 6] = new ChessPiece(ChessType.Pawn, ChessColor.White, x, 6);
+            SetPieceAt(x, 1, new ChessPiece(ChessType.Pawn, ChessColor.Black, x, 1));
+            SetPieceAt(x, 6, new ChessPiece(ChessType.Pawn, ChessColor.White, x, 6));
         }
-        board[0, 0] = new ChessPiece(ChessType.Rook, ChessColor.Black, 0, 0);
-        board[1, 0] = new ChessPiece(ChessType.Knight, ChessColor.Black, 1, 0);
-        board[2, 0] = new ChessPiece(ChessType.Bishop, ChessColor.Black, 2, 0);
-        board[3, 0] = new ChessPiece(ChessType.Queen, ChessColor.Black, 3, 0);
-        board[4, 0] = new ChessPiece(ChessType.King, ChessColor.Black, 4, 0);
-        board[5, 0] = new ChessPiece(ChessType.Bishop, ChessColor.Black, 5, 0);
-        board[6, 0] = new ChessPiece(ChessType.Knight, ChessColor.Black, 6, 0);
-        board[7, 0] = new ChessPiece(ChessType.Rook, ChessColor.Black, 7, 0);
-
-        board[0, 7] = new ChessPiece(ChessType.Rook, ChessColor.White, 0, 7);
-        board[1, 7] = new ChessPiece(ChessType.Knight, ChessColor.White, 1, 7);
-        board[2, 7] = new ChessPiece(ChessType.Bishop, ChessColor.White, 2, 7);
-        board[3, 7] = new ChessPiece(ChessType.Queen, ChessColor.White, 3, 7);
-        board[4, 7] = new ChessPiece(ChessType.King, ChessColor.White, 4, 7);
-        board[5, 7] = new ChessPiece(ChessType.Bishop, ChessColor.White, 5, 7);
-        board[6, 7] = new ChessPiece(ChessType.Knight, ChessColor.White, 6, 7);
-        board[7, 7] = new ChessPiece(ChessType.Rook, ChessColor.White, 7, 7);
+        SetPieceAt(0, 7, new ChessPiece(ChessType.Rook, ChessColor.White, 0, 7));
+        SetPieceAt(1, 7, new ChessPiece(ChessType.Knight, ChessColor.White, 1, 7));
+        SetPieceAt(2, 7, new ChessPiece(ChessType.Bishop, ChessColor.White, 2, 7));
+        SetPieceAt(3, 7, new ChessPiece(ChessType.Queen, ChessColor.White, 3, 7));
+        SetPieceAt(4, 7, new ChessPiece(ChessType.King, ChessColor.White, 4, 7));
+        SetPieceAt(5, 7, new ChessPiece(ChessType.Bishop, ChessColor.White, 5, 7));
+        SetPieceAt(6, 7, new ChessPiece(ChessType.Knight, ChessColor.White, 6, 7));
+        SetPieceAt(7, 7, new ChessPiece(ChessType.Rook, ChessColor.White, 7, 7));
     }
 
     public void PrintBoard()
