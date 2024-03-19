4f3fa316334081d78336d3218c5dff6ab8a8c045
Alessandro Tedesco <alessandrotedd@gmail.com>
2024-03-19 23:50:43 +0100

    Add basic ChessPiece functions

--- Assets/Chess/Scripts/ChessPiece.cs
+++ Assets/Chess/Scripts/ChessPiece.cs
@@ -8,11 +8,78 @@ public class ChessPiece
     [SerializeField]
     public ChessColor color;
 
-    public void ChessPieceSelected() {
-        Debug.Log("ChessPiece - ChessPieceSelected");
+    int column = -1;
+    int row = -1;
+
+    public ChessPiece(ChessType type, ChessColor color) {
+        this.type = type;
+        this.color = color;
     }
 
-    public void ChessPieceDeselected() {
-        Debug.Log("ChessPiece - ChessPieceDeselected");
+    public ChessPiece(ChessType type, ChessColor color, int column, int row) {
+        this.type = type;
+        this.color = color;
+        this.column = column;
+        this.row = row;
+    }
+
+    public void SetPosition(int x, int y) {
+        column = x;
+        row = y;
+    }
+
+    public string GetSymbol() {
+        switch (type) {
+            case ChessType.King:
+                if (color == ChessColor.Black) {
+                    return "♔";
+                } else {
+                    return "♚";
+                }
+                break;
+            case ChessType.Queen:
+                if (color == ChessColor.Black) {
+                    return "♕";
+                } else {
+                    return "♛";
+                }
+                break;
+            case ChessType.Rook:
+                if (color == ChessColor.Black) {
+                    return "♖";
+                } else {
+                    return "♜";
+                }
+                break;
+            case ChessType.Bishop:
+                if (color == ChessColor.Black) {
+                    return "♗";
+                } else {
+                    return "♝";
+                }
+                break;
+            case ChessType.Knight:
+                if (color == ChessColor.Black) {
+                    return "♘";
+                } else {
+                    return "♞";
+                }
+                break;
+            case ChessType.Pawn:
+                if (color == ChessColor.Black) {
+                    return "♙";
+                } else {
+                    return "♟";
+                }
+                break;
+            default:
+                break;
+        }
+        return "";
+    }
+
+    public void MoveTo(int x, int y) {
+        column = x;
+        row = y;
     }
 }
---
+++ Assets/Tests/ChessPieceTests.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class ChessPieceTests : MonoBehaviour
+{
+    [Test]
+    public void Test()
+    {
+        Assert.Pass();
+    }
+}
