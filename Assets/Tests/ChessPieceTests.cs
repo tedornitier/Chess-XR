9d114948cebee0dcc3bdcbdccd44b4f57622194d
Alessandro Tedesco <alessandrotedd@gmail.com>
2024-03-20 22:45:16 +0100

    Add rook moves test

--- Assets/Tests/ChessPieceTests.cs
+++ Assets/Tests/ChessPieceTests.cs
@@ -41,7 +41,7 @@ public class ChessPieceTests : MonoBehaviour
 
         /*0 1 2 3 4 5 6 7
         4[♙]♕ □ □ □ □ □ □
-        5 x x □ □ □ □ □ □
+        5 x # □ □ □ □ □ □
         6 ♖ □ □ □ □ □ □ □*/
         board.MovePiece(0, 3, 0, 4);
         CollectionAssert.AreEquivalent(new HashSet<(int, int)> {
@@ -77,5 +77,63 @@ public class ChessPieceTests : MonoBehaviour
         board.MovePiece(0, 6, 0, 7);
         Assert.AreEqual(0, piece.GetPossibleMoves(board).Count);
         board.PrintBoard();
+    }
+
+    [Test]
+    public void GetRookPossibleMovesTest()
+    {
+        /*0 1 2 3 4 5 6 7
+        0 □ □ x □ □ □ □ □
+        1 x x[♖]x x x x x
+        2 □ □ x □ □ □ □ □
+        3 □ □ x □ □ □ □ □
+        4 □ ♕ x □ □ □ □ □
+        5 □ ♛ x □ □ □ □ □
+        6 □ □ x □ □ □ □ □
+        7 □ □ x □ □ □ □ □*/
+        ChessPiece piece = new ChessPiece(ChessType.Rook, ChessColor.Black);
+        ChessBoard board = new ChessBoard();
+        board.SetPieceAt(2, 1, piece);
+        board.SetPieceAt(1, 4, new ChessPiece(ChessType.Queen, ChessColor.Black));
+        board.SetPieceAt(1, 5, new ChessPiece(ChessType.Queen, ChessColor.White));
+        board.PrintBoard();
+        CollectionAssert.AreEquivalent(new HashSet<(int, int)> {
+            (2, 0), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7),
+            (0, 1), (1, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1)
+        }, piece.GetPossibleMoves(board));
+
+        // move the rook to 2 4
+        /*0 1 2 3 4 5 6 7
+        0 □ □ x □ □ □ □ □
+        1 □ □ x □ □ □ □ □
+        2 □ □ x □ □ □ □ □
+        3 □ □ x □ □ □ □ □
+        4 □ ♕[♖]x x x x x
+        5 □ ♛ x □ □ □ □ □
+        6 □ □ x □ □ □ □ □
+        7 □ □ x □ □ □ □ □*/
+        board.MovePiece(2, 1, 2, 4);
+        board.PrintBoard();
+        CollectionAssert.AreEquivalent(new HashSet<(int, int)> {
+            (2, 0), (2, 1), (2, 2), (2, 3), (2, 5), (2, 6), (2, 7),
+            (3, 4), (4, 4), (5, 4), (6, 4), (7, 4)
+        }, piece.GetPossibleMoves(board));
+
+        // move the rook to 2 5
+        /*0 1 2 3 4 5 6 7
+        0 □ □ x □ □ □ □ □
+        1 □ □ x □ □ □ □ □
+        2 □ □ x □ □ □ □ □
+        3 □ □ x □ □ □ □ □
+        4 □ ♕ x □ □ □ □ □
+        5 □ #[♖]x x x x x
+        6 □ □ x □ □ □ □ □
+        7 □ □ x □ □ □ □ □*/
+        board.MovePiece(2, 4, 2, 5);
+        board.PrintBoard();
+        CollectionAssert.AreEquivalent(new HashSet<(int, int)> {
+            (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 6), (2, 7),
+            (1, 5), (3, 5), (4, 5), (5, 5), (6, 5), (7, 5)
+        }, piece.GetPossibleMoves(board));
     }
 }
