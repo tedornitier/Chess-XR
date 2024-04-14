e227c9538b07b003cbf891e2ea1a1003e92b5691
Alessandro Tedesco <alessandrotedd@gmail.com>
2024-04-14 14:40:11 +0200

    Add ChessAI tests

--- Assets/Tests/ChessAITests.cs
+++ Assets/Tests/ChessAITests.cs
@@ -6,38 +6,116 @@ using NUnit.Framework;
 public class ChessAITests
 {
     [Test]
-    public void Test()
+    public void QueenMoves_CapturesPawn_DoesntGetCaptured()
     {
-        ChessAI ai = new ChessAI(ChessColor.Black, 3);
+        ChessAI ai = new ChessAI(ChessColor.White, 2);
+
         ChessBoard board = new ChessBoard();
-        board.SetupBoard();
+        foreach (var position in new List<(int, int)> {
+            (1, 1), (2, 1), (0, 2), (1, 3), (1, 4), (0, 5), (2, 3)
+        }) {
+            board.SetPieceAt(position, new ChessPiece(ChessType.Pawn, ChessColor.Black));
+        }
+        board.SetPieceAt((0, 3), new ChessPiece(ChessType.Queen, ChessColor.White));
         board.PrintBoard();
 
-        board.MovePiece((3, 6), (3, 4));
+        List<(int, int, int, int)> legalMoves = ai.GenerateLegalMoves(board, ChessColor.White);
+        List<(int, int, int, int)> expectedLegalMoves = new List<(int, int, int, int)> {
+            (0, 3, 0, 2), (0, 3, 1, 2), (0, 3, 2, 1), (0, 3, 1, 3),
+            (0, 3, 1, 4), (0, 3, 0, 4), (0, 3, 0, 5)
+        };
+        Assert.AreEqual(expectedLegalMoves.Count, legalMoves.Count);
+        foreach (var move in expectedLegalMoves)
+        {
+            if (!legalMoves.Contains(move))
+            {
+                Debug.Log("Expected move: " + move + " not found");
+            }
+            Assert.IsTrue(legalMoves.Contains(move));
+        }
+
+        (int, int, int, int) bestMove = ai.GetBestMove(board);
+        (int, int, int, int) expectedBestMove = (0, 3, 2, 1);
+        Assert.AreEqual(expectedBestMove, bestMove);
+    }
+
+    [Test]
+    public void WhitePawnMoves_CapturesBlackPawn_DoesntGetCaptured()
+    {
+        ChessAI ai = new ChessAI(ChessColor.White, 2);
+
+        ChessBoard board = new ChessBoard();
+        board.SetPieceAt((0, 2), new ChessPiece(ChessType.Pawn, ChessColor.Black));
+        board.SetPieceAt((1, 3), new ChessPiece(ChessType.Pawn, ChessColor.Black));
+        board.SetPieceAt((3, 3), new ChessPiece(ChessType.Pawn, ChessColor.Black));
+        board.SetPieceAt((2, 4), new ChessPiece(ChessType.Pawn, ChessColor.White));
         board.PrintBoard();
 
-        (int, int, int, int) move = ai.GetBestMove(board);
-        board.MovePiece((move.Item1, move.Item2), (move.Item3, move.Item4));
+        (int, int, int, int) bestMove = ai.GetBestMove(board);
+        (int, int, int, int) expectedBestMove = (2, 4, 3, 3);
+        Assert.AreEqual(expectedBestMove, bestMove);
+    }
+
+    [Test]
+    public void WhiteKnightMoves_DoesntCapture_DoesntGetCaptured()
+    {
+        ChessAI ai = new ChessAI(ChessColor.White, 2);
+
+        ChessBoard board = new ChessBoard();
+        board.SetPieceAt((7, 7), new ChessPiece(ChessType.Knight, ChessColor.White));
+        board.SetPieceAt((4, 5), new ChessPiece(ChessType.Pawn, ChessColor.Black));
+        board.SetPieceAt((5, 6), new ChessPiece(ChessType.Pawn, ChessColor.Black));
         board.PrintBoard();
 
-        board.MovePiece((4, 6), (4, 4));
+        (int, int, int, int) bestMove = ai.GetBestMove(board);
+        (int, int, int, int) expectedBestMove = (7, 7, 6, 5);
+        Assert.AreEqual(expectedBestMove, bestMove);
+    }
+
+    [Test]
+    public void EvaluateTest()
+    {
+        ChessAI chessAI = new ChessAI(ChessColor.White, 2);
+        ChessBoard board = new ChessBoard();
+        foreach (var position in new List<(int, int)> {
+            (1, 1), (2, 1), (0, 2), (1, 3), (1, 4), (0, 5), (2, 3)
+        }) {
+            board.SetPieceAt(position, new ChessPiece(ChessType.Pawn, ChessColor.Black));
+        }
+        board.SetPieceAt((0, 3), new ChessPiece(ChessType.Queen, ChessColor.White));
         board.PrintBoard();
 
-        move = ai.GetBestMove(board);
-        board.MovePiece((move.Item1, move.Item2), (move.Item3, move.Item4));
+        int expectedScore = 2;
+        Assert.AreEqual(expectedScore, chessAI.Evaluate(board));
+
+        board.MovePiece((0, 3), (0, 2));
+        board.PrintBoard();
+        expectedScore = 3;
+        Assert.AreEqual(expectedScore, chessAI.Evaluate(board));
+
+        board.MovePiece((2, 1), (0, 2));
+        board.PrintBoard();
+        expectedScore = -6;
+        Assert.AreEqual(expectedScore, chessAI.Evaluate(board));
+    }
+
+    [Test]
+    public void SimulateMoveTest()
+    {
+        ChessAI chessAI = new ChessAI(ChessColor.White, 2);
+        ChessBoard board = new ChessBoard();
+        foreach (var position in new List<(int, int)> {
+            (1, 1), (2, 1), (0, 2), (1, 3), (1, 4), (0, 5), (2, 3)
+        }) {
+            board.SetPieceAt(position, new ChessPiece(ChessType.Pawn, ChessColor.Black));
+        }
+        board.SetPieceAt((0, 3), new ChessPiece(ChessType.Queen, ChessColor.White));
         board.PrintBoard();
 
-        board.MovePiece((5, 6), (5, 5));
-        board.PrintBoard();
-
-        List<(int, int, int, int)> legalMoves = ai.GenerateLegalMoves(board);
-        Assert.IsFalse(legalMoves.Contains((7, 1, 5, 7)));
-        Assert.IsFalse(legalMoves.Contains((7, 1, 7, 7)));
-        Assert.IsTrue(legalMoves.Contains((7, 1, 7, 2)));
-        Assert.IsTrue(legalMoves.Contains((7, 1, 7, 3)));
-
-        move = ai.GetBestMove(board);
-        board.MovePiece((move.Item1, move.Item2), (move.Item3, move.Item4));
-        board.PrintBoard();
+        (int, int, int, int) move = (0, 3, 2, 1);
+        ChessBoard newBoard = chessAI.SimulateMove(board, move);
+        newBoard.PrintBoard();
+        Assert.AreEqual(ChessType.Queen, newBoard.GetPieceAt(2, 1).type);
+        Assert.AreEqual(ChessType.Queen, board.GetPieceAt(0, 3).type);
     }
 }
